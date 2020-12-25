de6713c78bb11212b3eeb5ca924acbd17b461b9c
Shravan <shravan.akku@gmail.com>
2020-12-25 16:36:23 +0530

    MongDb connection

---
+++ L2_Db/index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const dotEnv = require("dotenv");
+
+dotEnv.config();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended:true,
+}));
+
+mongoose
+    .connect(process.env.DATABASE_URL,{
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        }
+    )
+    .then(()=>console.log("connected to db"))
+    .catch((err)=>console.log(err));
+
+app.listen(process.env.PORT, () => console.log("Server is up and running"));
